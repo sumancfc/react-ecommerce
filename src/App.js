96af9851c5d59a680d9d663ded6c0983b62edc90
sumancfc <sumanstha999@gmail.com>
2020-11-04 16:31:16 +0545

    Hero Section Slider Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import Footer from "./components/common/footer";
+import HeroSlider from "./components/common/Slider";
 
 function App() {
   return (
     <Router>
+      <HeroSlider />
       <Footer />
     </Router>
   );
---
+++ src/components/common/Slider/index.js
@@ -0,0 +1,77 @@
+import React from "react";
+import Slider from "react-slick";
+import { Link } from "react-router-dom";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./Slider.css";
+
+const HeroSlider = () => {
+  const settings = {
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+
+  let data = [
+    {
+      bgImg: "slider1.jpg",
+      sliderTitle: "Welcome to ecommerce",
+      brand: "rolex",
+      btnLink: "/shop",
+    },
+    {
+      bgImg: "slider2.jpg",
+      sliderTitle: "Welcome to ecommerce",
+      brand: "rolex",
+      btnLink: "/shop",
+    },
+    {
+      bgImg: "slider3.jpg",
+      sliderTitle: "Welcome to ecommerce",
+      brand: "rolex",
+      btnLink: "/shop",
+    },
+  ];
+
+  return (
+    <section className='p-0 slider'>
+      <Slider className='slide-1 home-slider' {...settings}>
+        {data.map((value, i) => {
+          return (
+            <div key={i}>
+              <div
+                className='home text-left p-left'
+                style={{
+                  backgroundImage: `url(assets/img/slider/${value.bgImg})`,
+                }}
+              >
+                <div className='container'>
+                  <div className='row'>
+                    <div className='col'>
+                      <div className='slider-contain'>
+                        <div>
+                          <h4>{value.sliderTitle}</h4>
+                          <h1>{value.brand}</h1>
+                          <Link
+                            to={value.btnLink}
+                            className='btn btn-outline btn-classic'
+                          >
+                            shop now
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </Slider>
+    </section>
+  );
+};
+
+export default HeroSlider;
