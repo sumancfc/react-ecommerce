dbe12b870f3868e02affbd06645f661016fa9677
sumancfc <sumanstha999@gmail.com>
2020-11-20 10:22:58 +0545

    Sticky header added

--- src/components/common/header/index.js
+++ src/components/common/header/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CategoryMenu from "./common/CategoryMenu";
 import MainMenu from "./common/MainMenu";
 import MiddleHeader from "./common/MiddleHeader";
@@ -8,6 +8,22 @@ import "./Header.css";
 import MobileHeader from "./MobileHeader";
 
 const Header = () => {
+  const [scroll, setScroll] = useState(0);
+  const [headerTop, setHeaderTop] = useState(0);
+
+  useEffect(() => {
+    const header = document.querySelector(".header__nav-sticky");
+    setHeaderTop(header.offsetTop);
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    setScroll(window.scrollY);
+  };
+
   return (
     <>
       {/* <!-- Header  -->  */}
@@ -17,7 +33,11 @@ const Header = () => {
           <TopHeader clsName='bg-black' />
           {/* Middle Header */}
           <MiddleHeader />
-          <div className='header__area-bottom header__nav-sticky background__red'>
+          <div
+            className={`header__area-bottom header__nav-sticky background__red ${
+              scroll > headerTop ? "stick" : ""
+            }`}
+          >
             <div className='container'>
               <div className='row align-items-center'>
                 <CategoryMenu />
