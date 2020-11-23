94d9e6eef0bbd71395f6a30f3ecac95b9822390e
sumancfc <sumanstha999@gmail.com>
2020-11-23 11:47:01 +0545

    Tab added on Login page

--- src/pages/Login-Register.js
+++ src/pages/Login-Register.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Tab from "react-bootstrap/Tab";
+import Nav from "react-bootstrap/Nav";
 import Breadcrumb from "../components/common/breadcrumb";
 import Button from "../components/common/button";
 import Layout from "../components/Layouts";
@@ -13,73 +15,81 @@ const LoginRegister = () => {
           <div className='row'>
             <div className='col-lg-7 col-md-12 ml-auto mr-auto'>
               <div className='login__register-wrapper'>
-                <div className='login__register-tab-list nav'>
-                  <a className='active' data-toggle='tab' href='#lg1'>
-                    <h4> login </h4>
-                  </a>
-                  <a data-toggle='tab' href='#lg2'>
-                    <h4> register </h4>
-                  </a>
-                </div>
-                <div className='tab-content'>
-                  <div id='lg1' className='tab-pane active'>
-                    <div className='login__form-wrapper'>
-                      <div className='login__register-form'>
-                        <form action='#' method='post'>
-                          <input
-                            type='text'
-                            name='name'
-                            placeholder='Username'
-                            required
-                          />
-                          <input
-                            type='password'
-                            name='password'
-                            placeholder='Password'
-                            required
-                          />
-                          <div className='button__box'>
-                            <div className='login__toggle-btn'>
-                              <input type='checkbox' />
-                              <label>Remember me</label>
-                              <Link to='/'>Forgot Password?</Link>
+                <Tab.Container defaultActiveKey='login'>
+                  <Nav className='login__register-tab-list'>
+                    <Nav.Item>
+                      <Nav.Link eventKey='login'>
+                        <h4>Login</h4>
+                      </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey='register'>
+                        <h4>Register</h4>
+                      </Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+
+                  <Tab.Content>
+                    <Tab.Pane eventKey='login'>
+                      <div className='login__form-wrapper'>
+                        <div className='login__register-form'>
+                          <form action='#' method='post'>
+                            <input
+                              type='text'
+                              name='name'
+                              placeholder='Username'
+                              required
+                            />
+                            <input
+                              type='password'
+                              name='password'
+                              placeholder='Password'
+                              required
+                            />
+                            <div className='button__box'>
+                              <div className='login__toggle-btn'>
+                                <input type='checkbox' />
+                                <label>Remember me</label>
+                                <Link to='/'>Forgot Password?</Link>
+                              </div>
+                              <Button type='submit'>Login</Button>
                             </div>
-                            <Button type='submit'>Login</Button>
-                          </div>
-                        </form>
+                          </form>
+                        </div>
                       </div>
-                    </div>
-                  </div>
-                  <div id='lg2' className='tab-pane'>
-                    <div className='login__form-wrapper'>
-                      <div className='login__register-form'>
-                        <form action='#' method='post'>
-                          <input
-                            type='text'
-                            name='name'
-                            placeholder='Username'
-                            required
-                          />
-                          <input
-                            type='password'
-                            name='password'
-                            placeholder='Password'
-                            required
-                          />
-                          <input
-                            name='email'
-                            placeholder='Email'
-                            type='email'
-                            required
-                          />
-                          <div className='button__box'>
-                            <Button type='submit'>Register</Button>
-                          </div>
-                        </form>
+                    </Tab.Pane>
+
+                    <Tab.Content eventKey='register'>
+                      <div className='login__form-wrapper'>
+                        <div className='login__register-form'>
+                          <form action='#' method='post'>
+                            <input
+                              type='text'
+                              name='name'
+                              placeholder='Username'
+                              required
+                            />
+                            <input
+                              type='password'
+                              name='password'
+                              placeholder='Password'
+                              required
+                            />
+                            <input
+                              name='email'
+                              placeholder='Email'
+                              type='email'
+                              required
+                            />
+                            <div className='button__box'>
+                              <Button type='submit'>Register</Button>
+                            </div>
+                          </form>
+                        </div>
                       </div>
-                    </div>
-                  </div>
-                </div>
+                    </Tab.Content>
+                  </Tab.Content>
+                </Tab.Container>
               </div>
             </div>
           </div>
